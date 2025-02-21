274a3576ea32e388677a1a2658505ba861675475
Subodh Mawle <smawle@uwm-cair.local.com>
2025-02-21 10:01:59 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-import pandas as pd 
-import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn
-import numpy as np
-import matplotlib.pyplot as plt
-import warnings
-from sklearn.preprocessing import LabelEncoder
-from sklearn.impute import KNNImputer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import f1_score
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.tree import plot_tree
+from imports import *
 
 warnings.filterwarnings('ignore')
 df= pd.read_csv('Position_Salaries.csv')
